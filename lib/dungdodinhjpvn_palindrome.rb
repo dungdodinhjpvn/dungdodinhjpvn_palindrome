b2e198687b3038d105a91d1e688615feb844b7ea
do.dinh.dung <SUN-ASTERISK\do.dinh.dung@b121736-lt.sun-asterisk.com>
2020-08-31 10:09:51 +0700

    Initialize repository

---
+++ lib/dungdodinhjpvn_palindrome.rb
@@ -0,0 +1,6 @@
+require "dungdodinhjpvn_palindrome/version"
+
+module DungdodinhjpvnPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "dungdodinhjpvn_palindrome"
+
+require "minitest/autorun"
